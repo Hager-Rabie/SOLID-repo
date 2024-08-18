acc202d3a2f2cd6f4d4833f973e6086692b61865
Hager Rabie <rabiehagar0@gmail.com>
2024-08-18 09:37:24 +0300

    Add files via upload

--- private.cpp
+++ private.cpp
@@ -1,216 +1,104 @@
-  #include <iostream>
-#include <string>
-using namespace std;
- 
-class Employee {
-public:
-    string name;
-    int age;
-    string position;
-    virtual ~Employee(){}
+  #include<bits/stdc++.h>
+   using namespace std;
+    void FOI () {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class GeometricShape{
+  public:
+  GeometricShape(){
+
+  }
+ virtual  double  area() =0;
+  virtual double perimeter() =0;
+  virtual ~GeometricShape(){
+
+  }
 };
-  class hireEmployeeservice : public Employee{
-    public:
-    void hireEmployee() {
-        cout << "Hiring employee: " << name << endl;
+  class  Triangle : public GeometricShape{
+  private:
+  double height , base , side;
+  public:
+   Triangle() : height(0) , base(0) , side(0)
+   {
+
+   }
+    Triangle(const double & h , const double & b ,const double &s) : height(h) , base(b) , side(s)
+    {
+
+    }
+    double area(){
+        return (.5*height*base);
+    }
+    double perimeter(){
+        return (height+base+side);
+    }
+    void set_dimensions(double h  , double b){
+  height = h ;
+  base = b ;
+    }
+      void set_dimensions(double hy){
+        side = hy ;
+    }
+   friend void  display_or_modify(double h , double b , double s ,  Triangle t){
+
+    t.height = h ;
+    t.base = b ;
+    t.side = s;
+    cout << t.height << ' ' <<t.base<< ' ' << t.side << endl;
+   }
+   bool operator ==(Triangle T1){
+   return (T1.area()==area());
+   }
+    ~ Triangle(){
+
     }
   };
-  class  FireEmployeeservice : public Employee{
-      void fireEmployee() {
-        cout << "Firing employee: " << name << endl;
-    }
-  };
-  
-   class  Employeeinfo : public Employee {
-    public : 
-    void printDetails() {
-        cout << "Name: " << name << ", Age: " << age << ", Position: " << position << endl;
-    }
-   };
-   class  EmployeePay : public Employee {
-    public:
-    void calculatePay() {
-        cout << "Calculating pay for: " << name << endl;
-    }
-   };
-   class  Employeepromotion : public Employee{
-    void promoteEmployee() {
-        cout << "Promoting employee: " << name << endl;
-    }
-   };
+ class Ellipse : public GeometricShape{
+    private:
+  const double  pi = 3.141;
+   double a , b ; // major& minor axes
+   public : 
+    Ellipse() : a(0) , b(0)
+    {
 
-class Manager : public Employee {
-public:
-    int teamSize;
-    void manageTeam() {
-        cout << "Managing a team of size: " << teamSize << endl;
     }
-};
-   class Teamadd {
-    public :
-    void addTeamMember() {
-        cout << "Adding team member" << endl;
-    }
-   };
-   class Teamremove  {
-    public: 
-    void removeTeamMember() {
-        cout << "Removing team member" << endl;
-    }
-   };
-   class Meeting  {
-    public:
-    void scheduleMeeting() {
-        cout << "Scheduling a meeting" << endl;
-    }
-   };
-   class Performance{
-    public :
-    void reviewPerformance() {
-        cout << "Reviewing performance" << endl;
-    }
-   };
+  Ellipse(const double &a1 , const double &b1) : a(a1) , b(b1) {
 
-class Report {
-public:
-    virtual void generateReport()=0;
-    virtual ~Report() {}
-};
- class ReportPrint  {
-    public :
-    void printReport() {
-        cout << "Printing report" << endl;
-    }
+  }
+  double area(){
+    return (pi*.5*a*.5*b);
+  }
+  double perimeter(){
+    double a2= a/2 , b2 = b/2;
+    return (2*pi*(sqrt((a2*a2+b2*b2)/2)));
+  }
+  void set_get(double a1,  double b1){
+      a = a1 , b = b1;
+      cout << a << " " << b << endl;
+  }
+    void set_get(double a1){
+      a = a1 ;
+      cout << a << " " << b << endl;
+  }
+  ~Ellipse(){
+
+  }
  };
- class ReportSave  {
-    void saveReport() {
-        cout << "Saving report" << endl;
-    }
- };
- class ReportDelete {
-    public : 
-    void deleteReport() {
-        cout << "Deleting report" << endl;
-    }
-  };
-  class ReportSend {
-    public :
-    void sendReport() {
-        cout << "Sending report" << endl;
-    }
-  };
-
-class PDFReport : public Report {
-public:
-    void generateReport() override {
-        cout << "Generating PDF report with special format" << endl;
-    }
-};
-
-class PayrollSystem {
-public:
-    void processPayroll() {
-        cout << "Processing payroll" << endl;
-    }
-};
-class Payslipgen {
-    public:
-    void generatePayslip() {
-        cout << "Generating payslip" << endl;
-    }
-};
-class PayslipSend{
-    public : 
-    void sendPayslip() {
-        cout << "Sending payslip" << endl;
-    }
-};
-  class Benefits {
-    public :
-    void handleEmployeeBenefits() {
-        cout << "Handling employee benefits" << endl;
-    }
-  };
-  class Taxes  {
-    public :
-    void handleEmployeeTaxes() {
-        cout << "Handling employee taxes" << endl;
-    }
-  };
-
-class Logger {
-      public :
- virtual   void logInfo(string info) = 0;
-
-};
-class Error : public Logger{
-    public:
-    void logError(string error) {
-        cout << "Logging error: " << error << endl;
-    }
-     void logInfo(string info) override {
-        cout << "Logging info: " << info << endl;
-    }
-
-};
- class Warning : public Logger{
-    public : 
-    void logWarning(string warning) {
-        cout << "Logging warning: " << warning << endl;
-    }
-     void logInfo(string info) override {
-        cout << "Logging info: " << info << endl;
-    }
-
- };
-
- class Debug: public Logger {
-    public :
-    void logDebug(string debug) {
-        cout << "Logging debug: " << debug << endl;
-    }
-     void logInfo(string info) override {
-        cout << "Logging info: " << info << endl;
-    }
-
- };
- class Trace : public Logger{
-    public :
-    void logTrace(string trace) {
-        cout << "Logging trace: " << trace << endl;
-    }
-     void logInfo(string info) override {
-        cout << "Logging info: " << info << endl;
-    }
-
- };
-
-int main() {
-    
-    Employee emp;
-    emp.name = "John Doe";
-    emp.age = 30;
-    emp.position = "Developer";
-    hireEmployeeservice emp2;
-    emp2.hireEmployee();
-    EmployeePay emp3;
-    emp3.calculatePay();
-
-    Manager mgr;
-    mgr.name = "Jane Smith";
-    mgr.teamSize = 10;
-    mgr.manageTeam();
-
-    PDFReport report;
-    report.generateReport();
-    ReportPrint report2;
-    report2.printReport();
-
-    PayrollSystem payroll;
-    payroll.processPayroll();
-
-    Error logger;
-    logger.logError("This is an error");
-
-    return 0;
+int main (){
+    FOI ();
+  Triangle t(4, 5, 7) , t2(2, 7, 8) , t1;
+  Ellipse e(4,5);
+ cout<< "area of triangle = " <<  t.area() << endl;
+ cout << "perimeter of triangle = " << t.perimeter() << endl;
+ if(t.area()==t2.area())
+   cout << "equal" << endl;
+   else
+   cout << "not equal" << endl;
+    cout<< "area of ellipes = " <<  e.area() << endl;
+ cout << "perimeter of ellipes = " << e.perimeter() << endl;
+     display_or_modify(12, 5 , 7, t1);
+return 0;
 }
